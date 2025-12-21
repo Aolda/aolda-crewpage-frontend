d18bc3c989bf7b30e2458a842f039644d71de7ab
Euncheol <euncheol0206@gmail.com>
2025-12-21 21:20:30 +0900

    Refactor App component to use Test page; add Test component with Badge and HashTag elements, and create corresponding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import './App.css'
-import Badge from './component/atoms/Badge'
-import HashTag from './component/atoms/HashTag'
+import Test from './pages/test'
 
 function App() {
 
   return (
     <>
-      <div>
-        <Badge $theme="status">활동중</Badge>
-        <Badge $theme="info">0기</Badge>
-        <Badge $theme="info">소프트웨어 22학번</Badge>
-      </div>
-
-      <div>
-        <HashTag completed $bgColor='#1A8EE5'>#</HashTag>
-        <HashTag date='2025.08.15' $bgColor='black'>#</HashTag>
-      </div>
+      <Test />
     </>
   )
 }
---
+++ src/pages/Test.tsx
@@ -0,0 +1,23 @@
+import './Test.css'
+import Badge from "../component/atoms/Badge";
+import HashTag from "../component/atoms/HashTag";
+
+function Test() {
+    return (
+        <>
+            <div className='container'>
+                <Badge $theme="status">활동중</Badge>
+                <Badge $theme="info">0기</Badge>
+                <Badge $theme="info">소프트웨어 22학번</Badge>
+            </div>
+
+            <div className='container'>
+                <HashTag completed $bgColor='#1A8EE5'>#</HashTag>
+                <HashTag $bgColor='#1A8EE5'>#</HashTag>
+                <HashTag date='2025.08.15' $bgColor='black'>#</HashTag>
+            </div>
+        </>
+    );
+}
+
+export default Test
