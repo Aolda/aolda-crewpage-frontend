abaa851b1128307fb735db5ff1ffb888ff3e8195
Euncheol <euncheol0206@gmail.com>
2026-01-27 23:11:21 +0900

    [Refactor] Adjust Footer height to use a more precise rem unit for improved layout consistency

--- src/components/organisms/Footer.styles.tsx
+++ src/components/organisms/Footer.styles.tsx
@@ -6,7 +6,7 @@ export const StyledFooter = styled.footer`
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 14.4375rem;
+    height: 14.375rem;
 
     background-color: #061C2C;
 
