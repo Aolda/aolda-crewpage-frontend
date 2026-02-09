c26cb3a89f250d1f9a58970ae63d3ed4f061fc47
Euncheol <euncheol0206@gmail.com>
2026-02-09 17:47:09 +0900

    Refactor import path for CrewBlock component from molecules to organisms for better component organization.

--- src/components/templates/crew/CrewPagetemplate.tsx
+++ src/components/templates/crew/CrewPagetemplate.tsx
@@ -4,7 +4,7 @@ import React, { useState, useMemo } from 'react';
 import { CrewMember, POSITION_LABEL } from '@/types/crew';
 import * as S from './CrewPageTemplate.styles';
 import BaseTemplate from '@/components/templates/BaseTemplate/BaseTemplate';
-import CrewBlock from '@/components/molecules/CrewBlock';
+import CrewBlock from '@/components/organisms/CrewBlock';
 import SearchBox from '@/components/molecules/SearchBox';
 import Select from '@/components/molecules/Select';
 
