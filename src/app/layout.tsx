0446b3d9d6a00ce52a088377345ef0a412c516a7
Euncheol <euncheol0206@gmail.com>
2026-02-19 21:04:14 +0900

    [Refactor] Apply Noto Sans KR in layout and update metadata icons structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from 'next/font/google';
 import StyledComponentsRegistry from '@/lib/registry';
 // import { MSWComponent } from '@/components/MSWComponent';
 import Header from "@/components/organisms/Header";
 import Footer from "@/components/organisms/Footer";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+const notoScript = Noto_Sans_KR({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-noto-sans', 
 });
 
 export const metadata: Metadata = {
     title: "Aolda",
     description: "Aolda",
-    icons: "/images/aoldaIcon.svg",
+    icons: {
+        icon: [
+            {
+                url: "/images/aoldaIcon.svg?v=1",
+                type: "image/svg+xml", // SVG 타입을 명시적으로 선언
+            },
+        ],
+        // 모바일 기기 홈 화면 추가용 아이콘
+        apple: "/images/aoldaIcon.svg",
+    },
 };
 
 export default function RootLayout({
@@ -28,16 +33,16 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="ko">
-        <body>
-            <StyledComponentsRegistry>
-                <Header />
-                {/* <MSWComponent> */}
-                    <main style={{"marginBottom": "5rem", "minHeight": "80vh"}}>{children}</main>
-                {/* </MSWComponent> */}
-                <Footer />
-            </StyledComponentsRegistry>
-        </body>
+        <html lang="ko" className={`${notoScript.variable}`}>
+            <body className={notoScript.className}>
+                <StyledComponentsRegistry>
+                    <Header />
+                    {/* <MSWComponent> */}
+                        <main style={{"marginBottom": "5rem", "minHeight": "80vh"}}>{children}</main>
+                    {/* </MSWComponent> */}
+                    <Footer />
+                </StyledComponentsRegistry>
+            </body>
         </html>
     );
 }
